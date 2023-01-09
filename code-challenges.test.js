ed4952487f985ebf54286cf6079714eeaa1e1f83
romeoyutig <romeoyutig2@outlook.com>
2023-01-09 08:05:49 -0800

    attempted assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,8 @@
 // ASSESSMENT 4: JavaScript Coding Practical Questions with Jest
 
+
+
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -15,6 +18,12 @@
 // a) Create a test with an expect statement using the variable provided.
 // HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
+// pseudo code: declare a function named colorFilter
+// input: array of strings
+// output:a modified array with the first element removed, and the rest randomized.
+// process: declare a variable within the colorFilter function that takes the parameter and slices the first value from the array.
+// return the sliced array and randomize using the method .sort(), and Math.random 
+
 const colors1 = ["purple", "blue", "green", "yellow", "pink"]
 // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
 const colors2 = [
@@ -27,25 +36,100 @@ const colors2 = [
 ]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
+describe("color_Filter", () => {
+  it("returns an array with the first item remove, then shuffles the array", () => {
+    expect(["purple", "blue", "green", "yellow", "pink"]).toEqual(expect.arrayContaining(colors1))
+})
+})
+
+describe("color_Filter", () => {
+  it("returns an array with the first item remove, then shuffles the array", () => {
+    expect(colors2).toEqual(expect.arrayContaining(colors2))
+})
+})
+// ReferenceError: color_Filter is not defined
+// good test
+
+const colorFilter = (array) => {
+  let newArray = array.slice(1)
+  return newArray.sort(() => 0.5 - Math.random)
+}
+colorFilter(colors1)
+
 // b) Create the function that makes the test pass.
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
 // a) Create a test with expect statements for each of the variables provided.
 
+// pseudo code: declare a function named voteCounter
+
+// input: object
+// output: number of votes subtracted by number of downvotes
+// process: pass in the paramter "votes" into the function 
+// have the upvotes be subtracted by the downvotes
+
+
 const votes1 = { upVotes: 13, downVotes: 2 }
 // Expected output: 11
 const votes2 = { upVotes: 2, downVotes: 33 }
 // Expected output: -31
 
+describe ("voteCounter", () => {
+  it("contains up votes and down votes and returns the end tally.", () => {
+    expect(voteCounter(votes1)).toEqual(
+      11
+    )
+  })
+})
+
+describe ("voteCounter", () => {
+  it("contains up votes and down votes and returns the end tally.", () => {
+    expect(voteCounter(votes2)).toEqual(
+      -31
+    )
+  })
+})
+
+// ReferenceError: voteCounter is not defined
+// good test, voteCounter function has not been created yet.
+
 // b) Create the function that makes the test pass.
 
-// --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
+const voteCounter = (votes) => {
+  return votes.upVotes - votes.downVotes
+}
+// --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments. 
 
-// a) Create a test with an expect statement using the variables provided.
+// a) Create a test with an expect statement using the variables provided. 
+
+// pseudo code:
+// declare a function called newArray
+// input: two arrays
+// output: a single array with every unique value of each element of the two arrays
+// process: declare a new array within a function called "noDuplicates"
+// concat the arrays that will be passed as arguments
+// use the method ...new Set to remove all duplicate values from the combined array
 
 const dataArray1 = ["array", "object", "number", "string", "Boolean"]
 const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
 // Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
 
+describe("newArray", () => {
+  it ("takes in two arrays as arguments and returns one array with no duplicate values", () => {
+    expect(newArray(dataArray1, dataArray2)).toEqual(["array", "object", "number", "string", "Boolean", "null", "undefined"])
+  })
+})
+
+// ReferenceError: newArray is not defined
+// good test, function isn't made yet
+
 // b) Create the function that makes the test pass.
+
+const newArray = (array1, array2) => {
+  let noDuplicates = (array1.concat(array2))
+  return [...new Set(noDuplicates)]
+  }
+
+
+
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -10,6 +10,27 @@ num2 = 42
 num3 = 221
 # Expected output: '221 is odd'
 
+# pseudo code: 
+# create a method named even or odds
+# input: number
+# output: string saying "'num' is odd or even"
+# process: declare a function with a number as a parameter
+# use the method .even? and .odd? to check parameters divisibility by 2
+# print a string saying whether the parameter is even or odd
+# use string interpolation on the return
+
+def evens_or_odds(num)
+  if num.even?
+    p "#{num} is even"
+  else num.odd?
+    p "#{num} is odd"
+  end
+end
+evens_or_odds(num1)
+evens_or_odds(num2)
+evens_or_odds(num3)
+
+
 
 
 # -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. 
@@ -22,9 +43,30 @@ beatles_album2 = 'Sgt Pepper'
 beatles_album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
 
+# create a method
+# input: string
+# expected output: string with the vowels removed
+# process: use .delete with vowels provided in the argument to delete from each parameter
+
+def vowel_deleter(string)
+  p string.delete('aeiouAEIOU')
+end
+vowel_deleter(beatles_album1)
+vowel_deleter(beatles_album2)
+vowel_deleter(beatles_album3)
 
 
 # -------------------3) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forward or backward. Use the test variables provided.
+
+# pseudo code:
+# create a method named palindrome_method
+# input: string
+# input: string notifying user whether or not the string is a palindrome
+# process: use conditional
+# use methods .downcase to check for edgecases
+# use .reverse to check if string is a palindrome
+# return a string notifying whether string was a palindrome
+# use string interpolation providing the parameter 
 
 palindrome_tester1 = 'Racecar'
 # Expected output: 'Racecar is a palindrome'
@@ -32,3 +74,15 @@ palindrome_tester2 = 'LEARN'
 # Expected output: 'LEARN is not a palindrome'
 palindrome_tester3 = 'Rotator'
 # Expected output: 'Rotator is a palindrome'
+
+def palindrome_method(string)
+  if string.downcase == string.downcase.reverse
+    p "#{string} is a palindrome"
+  else
+    p "#{string} is not a palindrome"
+  end
+end
+
+palindrome_method(palindrome_tester1)
+palindrome_method(palindrome_tester2)
+palindrome_method(palindrome_tester3)
